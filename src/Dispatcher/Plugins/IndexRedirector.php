9ddd323521d88cab17e69440e084b9fec0535ad3
Mark Johnson <mark@techpivot.net>
2017-02-15 19:52:56 -0800

    IndexRedirector fixes to ensure redirection takes place only when necessary.

--- src/Dispatcher/Plugins/IndexRedirector.php
+++ src/Dispatcher/Plugins/IndexRedirector.php
@@ -10,13 +10,55 @@ use Phalcon\Mvc\User\Plugin;
 /**
  * IndexRedirector.
  *
- * Ensures that any dispatched route that includes the explicit default index, typically "index", as the
- * action or controller is prohibited. The default behavior of allowing the explicit default index is
- * a by product of Phalcon's routing system that will allow explicitly matched controllers and actions.
+ * Ensures that any dispatched route that dynamically matches the router's default action or handler
+ * is prohibited. Any explicitly defined route 'action' or 'controller' that matches a default
+ * is allowed.
  *
- * Prohibiting this behavior is useful in ensuring accurate Search Engine Optimization as well as
- * providing a level of increased security as default routes can expose information about the underlying
+ * Limiting the accessibility of the default routes is important in ensuring accurate Search Engine Optimization
+ * as well as providing a level of increased security as default routes can expose information about the underlying
  * system architecture.
+ *
+ * For example:
+ *
+ * <code>
+ *  $router = new Router(false);
+ *  $router->setUriSource(Router::URI_SOURCE_SERVER_REQUEST_URI);
+ *  $router->removeExtraSlashes(true);
+ *  $router->setDefaultAction('index');
+ *  $router->setDefaultController('index');
+ *
+ *  $router->add('/', []);
+ *  $router->add('/:controller/', [
+ *       'controller' => 1,
+ *  ]);
+ *  $router->add('/:controller/:action', [
+ *       'controller' => 1,
+ *       'action'     => 2
+ *  ]);
+ *
+ *  // Include this plugin
+ *  $di->setShared('dispatcher', function () {
+ *      $eventsManager = new EventsManager();
+ *      $eventsManager->attach('dispatch', new IndexRedirector());
+ *
+ *      $dispatcher = new Dispatcher();
+ *      $dispatcher->setEventsManager($eventsManager);
+ *
+ *      return $dispatcher;
+ *  }
+ * </code>
+ *
+ * The above router and dispatcher setup results in the following:
+ *
+ * <pre>
+ * URL Route                        Result
+ * ========================================================
+ * /index/index                     Not Matched (Exception)
+ * /valid-controller/valid-action   Matched
+ * /index                           Not Matched (Exception)
+ * /valid-controller                Matched
+ * /                                Matched
+ * </pre>
  */
 class IndexRedirector extends Plugin
 {
@@ -35,7 +77,8 @@ class IndexRedirector extends Plugin
     private $defaultHandlerIndex;
 
     /**
-     * IndexRedirectorPlugin constructor.
+     * IndexRedirector plugin constructor. Instantiate the default router action and handler
+     * values here as they are not publically retrievable from the Router instance.
      *
      * @param string $defaultActionIndex   The default action index. Defaults to "index"
      * @param string $defaultHandlerIndex  The default handler index. Defaults to "index"
@@ -57,37 +100,42 @@ class IndexRedirector extends Plugin
         /** @var \Phalcon\Mvc\RouterInterface $router */
         $router = $this->getDI()->getShared('router');
 
+        $matchedRoute =  $router->getMatchedRoute();
+        if ($matchedRoute === null) {
+            return;
+        }
+
+        $paths = $matchedRoute->getPaths();
+
         // Action Validation
-        switch ($router->getActionName()) {
-            case $this->defaultActionIndex:
-                $exception = new Exception('Action "index" was not found on handler "' .
-                    $dispatcher->getControllerName() . '"', Dispatcher::EXCEPTION_HANDLER_NOT_FOUND);
+        if ($router->getActionName() === $this->defaultActionIndex &&
+            isset($paths['action']) && $paths['action'] !== $this->defaultActionIndex
+        ) {
+            $exception = new Exception('Action "' . $this->defaultActionIndex . '" was not found on handler "' .
+                $dispatcher->getControllerName() . '"', Dispatcher::EXCEPTION_HANDLER_NOT_FOUND);
 
-                $eventsManager = $dispatcher->getEventsManager();
-                if ($eventsManager->fire('dispatch:beforeException', $dispatcher, $exception) === false) {
-                    break;
-                }
+            $eventsManager = $dispatcher->getEventsManager();
+            if ($eventsManager->fire('dispatch:beforeException', $dispatcher, $exception) === false) {
+                return false;
+            }
 
-                throw $exception;
+            throw $exception;
         }
 
         // Handler Validation
-        switch ($router->getControllerName()) {
-            case $this->defaultHandlerIndex:
-                // Allow the default pattern
-                if ($router->getMatchedRoute()->getPattern() === '/') {
-                    break;
-                }
+        if ($router->getControllerName() === $this->defaultHandlerIndex &&
+            isset($paths['controller']) && $paths['controller'] !== $this->defaultHandlerIndex
+        ) {
+            $exception = new Exception('"' . $dispatcher->getNamespaceName() .
+                '\\' . ucfirst($this->defaultHandlerIndex) . 'Controller" handler class cannot be loaded',
+                Dispatcher::EXCEPTION_HANDLER_NOT_FOUND);
 
-                $exception = new Exception('"' . $dispatcher->getNamespaceName() .
-                    '\IndexController" handler class cannot be loaded', Dispatcher::EXCEPTION_HANDLER_NOT_FOUND);
+            $eventsManager = $dispatcher->getEventsManager();
+            if ($eventsManager->fire('dispatch:beforeException', $dispatcher, $exception) === false) {
+                return false;
+            }
 
-                $eventsManager = $dispatcher->getEventsManager();
-                if ($eventsManager->fire('dispatch:beforeException', $dispatcher, $exception) === false) {
-                    break;
-                }
-
-                throw $exception;
+            throw $exception;
         }
     }
 }
